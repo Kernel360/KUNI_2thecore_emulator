cfaef7cf3da018c517410e70286c02c194b8732e
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-08-14 17:56:33 +0900

    - carEntity 리펙토링 - 여러 로직들 중 emulatorId 사용 변경 - swagger config 추가

--- src/main/java/com/example/emulator/infrastructure/car/CarRepository.java
+++ src/main/java/com/example/emulator/infrastructure/car/CarRepository.java
@@ -1,9 +1,7 @@
 package com.example.emulator.infrastructure.car;
 
 import com.example.emulator.car.domain.CarEntity;
-import com.example.emulator.car.CarStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,11 +13,5 @@ public interface CarRepository extends JpaRepository<CarEntity, Integer> {
 
     Optional<CarEntity> findByCarNumber(String carNumber);
 
-    Optional<CarEntity> findByEmulatorId(Integer emulatorId);
-
-    List<CarEntity> findByStatusIn(List<CarStatus> statuses); // 차량 상태 리스트 조회
-
-    @Query("select c.status, count(c) from Car c group by c.status")
-    List<Object[]> getCountByStatus();
-
+    List<CarEntity> findAllByLoginId(String loginId);
 }
