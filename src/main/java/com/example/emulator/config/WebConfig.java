3747b96c314209af92752e68359bd1d893fe0b24
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-08-14 18:01:57 +0900

    - carEntity 리펙토링 (#6)
    - 여러 로직들 중 emulatorId 사용 변경
    - swagger config 추가

--- src/main/java/com/example/emulator/config/WebConfig.java
+++ src/main/java/com/example/emulator/config/WebConfig.java
@@ -10,19 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:3001",
-                        "http://localhost:3002",
-                        "http://localhost:3003",
-                        "http://localhost:3004",
-                        "http://localhost:3005",
-                        "http://localhost:3006",
-                        "http://localhost:3007",
-                        "http://localhost:3008",
-                        "http://localhost:3009",
-                        "http://localhost:3010"
-                )
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
